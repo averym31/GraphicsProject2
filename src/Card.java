3a9eb7018d7280b4ce89e93a7355831ed162d257
Averym31 <averym31@nycstudents.net>
2026-05-19 08:34:33 -0400

    5/19

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,23 @@
 public class Card {
+    private String suit;
+    private String value;
+    private String filename;
+
+    public Card(String suit, String value){
+        this.suit = suit;
+        this.value = value;
+        filename = "card_" + suit + "_" + value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String toString(){
+        return "Suit: " + suit + " Value: " + value + " Filename: " + filename;
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> cardDeck;
+
+    public Deck(){
+        String[] suitList = {"clubs","diamonds","hearts","spades"};
+        String[] valueList = {"A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K"};
+        for (String string : suitList) {
+            for (String s : valueList) {
+                Card newCard = new Card(string, s);
+                cardDeck.add(newCard);
+            }
+        }
+    }
+
+    public Card getRandomCard(){
+        int randomInt = (int) (Math.random() * cardDeck.size());
+        Card randomCard = cardDeck.get(randomInt);
+        cardDeck.remove(cardDeck.get(randomInt));
+        return randomCard;
+    }
 }
