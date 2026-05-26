9087570db105cdd7d9c00b74cbcb7e2a05282960
Averym31 <averym31@nycstudents.net>
2026-05-26 08:44:33 -0400

    5/26

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -11,12 +11,14 @@ class DrawPanel extends JPanel implements MouseListener {
     Card[][] screen = new Card[3][3];
     private int numReset;
     Rectangle playAgainHitbox;
+    Rectangle replaceCardsHitbox;
     private ArrayList<Card> highlighted;
 
     public DrawPanel() {
         highlighted = new ArrayList<Card>();
         deck = new Deck();
         playAgainHitbox = new Rectangle(100, 300, 150, 50);
+        replaceCardsHitbox = new Rectangle(320, 10, 150, 50);
         for (int r = 0; r < screen.length; r++){
             for (int c = 0; c < screen[0].length; c++){
                 currentCard = deck.getRandomCard();
@@ -63,6 +65,8 @@ class DrawPanel extends JPanel implements MouseListener {
         Point p = e.getPoint();
         int button = e.getButton();
         // 1 = lclick, 3 = rclick
+
+        // when u click on the play again button
         if (playAgainHitbox.contains(p) && button == 1){
             numReset++;
             deck = new Deck();
@@ -74,13 +78,14 @@ class DrawPanel extends JPanel implements MouseListener {
             }
         }
 
+        // when u click on card
         for (int r = 0; r < screen.length; r++) {
             for (int c = 0; c < screen[0].length; c++) {
-                if (!deck.getCardDeck().isEmpty() && button == 1) {
-                    if (screen[r][c].getHitbox().contains(p)) {
-                        screen[r][c] = deck.getRandomCard();
-                    }
-                }
+//                if (!deck.getCardDeck().isEmpty() && button == 1) {
+//                    if (screen[r][c].getHitbox().contains(p)) {
+//                        screen[r][c] = deck.getRandomCard();
+//                    }
+//                }
                 if (button == 3 && screen[r][c].getHitbox().contains(p)) {
                     if (!highlighted.contains(screen[r][c])){
                         highlighted.add(screen[r][c]);
@@ -88,6 +93,16 @@ class DrawPanel extends JPanel implements MouseListener {
                     else {
                         highlighted.remove(screen[r][c]);
                     }
+                }
+            }
+        }
+
+        // when u click on the replace cards button
+        if (replaceCardsHitbox.contains(p) && button == 1){
+            if (highlighted != null){
+                for (int i = 0; i < highlighted.size(); i++){
+                    highlighted.remove(i);
+                    i--;
                 }
             }
         }
